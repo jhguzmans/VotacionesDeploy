81e06e23edc1c1a9156eeaedb1dde1f54611abb7
Julian Guzman <jhguzmans@unal.edu.co>
2024-03-21 17:11:12 -0500

    reestructurando back

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
-const PORT = 3001;
-
+require("dotenv").config();
+const { PORT } = process.env;
 conn
   .sync()
   .then(() => {
