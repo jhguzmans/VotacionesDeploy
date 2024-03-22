f3701e39f81e0f3eeabac0e0ab45a0053910d7ef
Julian Guzman <101350373+jhguzmans@users.noreply.github.com>
2024-03-22 12:35:43 -0500

    Update saveUsers.js

--- Server/src/handlers/saveUsers.js
+++ Server/src/handlers/saveUsers.js
@@ -3,7 +3,8 @@ const c_saveUsers = require("../controlers/c_saveUsers");
 
 const saveUsers = async (req, res) => {
   try {
-    const data = await c_saveUsers();
+    const { users } = req.body; // Suponiendo que el body contiene un objeto con una propiedad llamada 'users' que es un arreglo de objetos
+    const data = await c_saveUsers(users);
     return res.status(200).send(data);
   } catch (error) {
     return res.status(400).json({ error: error.message });
