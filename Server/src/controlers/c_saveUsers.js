0cc7369d182865a49fc743ec463b979cbb9b636d
Julian Guzman <jhguzmans@unal.edu.co>
2024-03-22 17:19:04 -0500

    ya debe guardar

--- Server/src/controlers/c_saveUsers.js
+++ Server/src/controlers/c_saveUsers.js
@@ -27,20 +27,73 @@
 // };
 
 // module.exports = c_saveUsers;
-const { User } = require("../db");
+// const { User } = require("../db");
 // const bcrypt = require("bcrypt");
 
-const c_saveUsers = async () => {
-  const workbook = xlsx.readFile(
-    "https://docs.google.com/spreadsheets/d/17Zv2UyvWa_4lYCvoZ6JYO833-SvMFYxD5yWEpxZc7d4/edit?usp=sharing"
-    //  "D:/Escritorio/baseDeDatosPruebaConjuntos.xlsx"
-  );
-  const sheetName = workbook.SheetNames[0];
-  const worksheet = workbook.Sheets[sheetName];
-  const data = xlsx.utils.sheet_to_json(worksheet);
-  if (!data) throw new Error("Error al guardar los datos en la DB");
+// const c_saveUsers = async () => {
+//   const workbook = xlsx.readFile(
+//     "https://docs.google.com/spreadsheets/d/17Zv2UyvWa_4lYCvoZ6JYO833-SvMFYxD5yWEpxZc7d4/edit?usp=sharing"
+//     //  "D:/Escritorio/baseDeDatosPruebaConjuntos.xlsx"
+//   );
+//   const sheetName = workbook.SheetNames[0];
+//   const worksheet = workbook.Sheets[sheetName];
+//   const data = xlsx.utils.sheet_to_json(worksheet);
+//   if (!data) throw new Error("Error al guardar los datos en la DB");
 
-  data.forEach(async (rowData) => {
+//   data.forEach(async (rowData) => {
+//     try {
+//       // const hashedPassword = bcrypt.hashSync(String(rowData.pass), 10);
+//       // rowData.pass = hashedPassword;
+//       await User.create(rowData);
+//     } catch (error) {
+//       console.error("Error creando entidad:", error);
+//       failedInserts.push(rowData);
+//     }
+//   }
+//   )
+//   return { failedInserts };
+// };
+
+// // module.exports = c_saveUsers;
+// const { User } = require("../db");
+// // const bcrypt = require("bcrypt");
+
+// const c_saveUsers = async () => {
+//   const workbook = xlsx.readFile(
+//     "https://docs.google.com/spreadsheets/d/17Zv2UyvWa_4lYCvoZ6JYO833-SvMFYxD5yWEpxZc7d4/edit?usp=sharing"
+//     //  "D:/Escritorio/baseDeDatosPruebaConjuntos.xlsx"
+//   );
+//   const sheetName = workbook.SheetNames[0];
+//   const worksheet = workbook.Sheets[sheetName];
+//   const data = xlsx.utils.sheet_to_json(worksheet);
+//   if (!data) throw new Error("Error al guardar los datos en la DB");
+
+//   data.forEach(async (rowData) => {
+//     try {
+//       // const hashedPassword = bcrypt.hashSync(String(rowData.pass), 10);
+//       // rowData.pass = hashedPassword;
+//       await User.create(rowData);
+//     } catch (error) {
+//       console.error("Error creando entidad:", error);
+//       failedInserts.push(rowData);
+//     }
+//   });
+//   return { failedInserts };
+// };
+
+// module.exports = c_saveUsers;
+
+const { User } = require("../db");
+
+const c_saveUsers = async (userData) => {
+  if (!userData || !Array.isArray(userData)) {
+    throw new Error("Los datos de usuario no son válidos");
+  }
+
+  const failedInserts = [];
+
+  for (let i = 0; i < userData.length; i++) {
+    const rowData = userData[i];
     try {
       // const hashedPassword = bcrypt.hashSync(String(rowData.pass), 10);
       // rowData.pass = hashedPassword;
--- Server/src/handlers/saveUsers.js
+++ Server/src/handlers/saveUsers.js
@@ -1,9 +1,23 @@
+// const axios = require("axios");
+// const c_saveUsers = require("../controlers/c_saveUsers");
+
+// const saveUsers = async (req, res) => {
+//   try {
+//     const { users } = req.body; // Suponiendo que el body contiene un objeto con una propiedad llamada 'users' que es un arreglo de objetos
+//     const data = await c_saveUsers(users);
+//     return res.status(200).send(data);
+//   } catch (error) {
+//     return res.status(400).json({ error: error.message });
+//   }
+// };
+// module.exports = saveUsers;
+
 const axios = require("axios");
 const c_saveUsers = require("../controlers/c_saveUsers");
 
 const saveUsers = async (req, res) => {
   try {
-    const { users } = req.body; // Suponiendo que el body contiene un objeto con una propiedad llamada 'users' que es un arreglo de objetos
+    const users = req.body; // Suponiendo que el body contiene un objeto con una propiedad llamada 'users' que es un arreglo de objetos
     const data = await c_saveUsers(users);
     return res.status(200).send(data);
   } catch (error) {
