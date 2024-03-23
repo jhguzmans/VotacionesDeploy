6e3980d6ade9b150df5d97e7f024a0da02f361a4
Julian Guzman <jhguzmans@unal.edu.co>
2024-03-23 10:03:15 -0500

    log del ultimasdasdasasdsahjbjd

--- client/src/Components/DetalleRespuestas/DetalleRespuestas.jsx
+++ client/src/Components/DetalleRespuestas/DetalleRespuestas.jsx
@@ -123,10 +123,10 @@ const DetalleRespuestas = () => {
       setChartInstance(newChartInstance);
     }
   }, [chartData]);
-  console.log(
-    "lo que descarga la ultima pregunta es: ",
-    ultimaPreguntaResponse
-  );
+  //   console.log(
+  //     "lo que descarga la ultima pregunta es: ",
+  //     ultimaPreguntaResponse
+  //   );
   return (
     <div className={styles.container}>
       <h2>Las respuestas son:</h2>
