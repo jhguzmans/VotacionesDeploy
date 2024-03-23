4f8fd4519ad2d8a9de99619860e4c7eeb7cefb4a
Julian Guzman <jhguzmans@unal.edu.co>
2024-03-23 08:06:48 -0500

    sin navbar en home

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,14 +1,18 @@
 // NavBar.jsx
 import React from "react";
-import styles from "./NavBar.module.css";
+//import styles from "./NavBar.module.css";
 
 const NavBar = ({ onLogout }) => {
   return (
-    <div className={styles.navbar}>
-      <span className={styles.welcome}>Bienvenido</span>
-      <button className={styles.logoutButton} onClick={onLogout}>
-        Cerrar sesión
-      </button>
+    //  <div className={styles.navbar}>
+    //    <span className={styles.welcome}>Bienvenido</span>
+    //    <button className={styles.logoutButton} onClick={onLogout}>
+    //      Cerrar sesión
+    //    </button>
+    //  </div>
+    <div>
+      <span>Bienvenido</span>
+      <button onClick={onLogout}>Cerrar sesión</button>
     </div>
   );
 };
