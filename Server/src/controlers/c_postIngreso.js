86bcac6d83e38781ea07fdbc70e43cf32d2da023
Julian Guzman <jhguzmans@unal.edu.co>
2024-03-22 22:32:24 -0500

    arreglando ingreso

--- Server/src/controlers/c_postIngreso.js
+++ Server/src/controlers/c_postIngreso.js
@@ -2,7 +2,10 @@ const { User } = require("../db");
 
 const c_postIngreso = async (username) => {
   try {
-    await User.update({ ingresa: true }, { where: { torreApto: username } });
+    await User.update(
+      { ingresa: true },
+      { where: { torreApto: this.toString(username) } }
+    );
     return "Entidad modificada correctamente";
   } catch (error) {
     console.log(error);
