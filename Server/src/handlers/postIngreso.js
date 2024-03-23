9e06815879c48f8deee0b823fb0cb3871c1756bd
Julian Guzman <jhguzmans@unal.edu.co>
2024-03-22 22:21:50 -0500

    arreglando ingreso

--- Server/src/handlers/postIngreso.js
+++ Server/src/handlers/postIngreso.js
@@ -2,7 +2,8 @@ const c_postIngreso = require("../controlers/c_postIngreso");
 const axios = require("axios");
 const postIngreso = async (req, res) => {
   try {
-    const { username } = req.params; // Aquí está el cambio    const usuarioquellega = await c_postIngreso(username);
+    const { username } = req.params; // Aquí está el cambio
+    const usuarioquellega = await c_postIngreso(username);
     return res.status(200).json(usuarioquellega);
   } catch (error) {
     return res.status(500).json({ error: error.message });
