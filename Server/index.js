ca569ce3c044c1b469154916b4ab5a028203c52c
Julian Guzman <jhguzmans@unal.edu.co>
2025-04-06 01:25:44 -0500

    fiorento3

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
+
 const { PORT } = process.env;
 conn
   .sync()
