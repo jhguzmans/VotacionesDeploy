8e0fc6de80e9afe5b49e91b7b7e26af4c2241c11
NIO <jhguzmans@unal.edu.co>
2026-03-11 15:02:33 -0500

    Recodo v4 Backend

--- Server/index.js
+++ Server/index.js
@@ -1,46 +1,15 @@
-// const axios = require("axios");
-// const server = require("./src/server");
-// const { conn } = require("./src/db.js");
-// require("dotenv").config();
-
-// const { PORT } = process.env;
-// conn
-//   .sync()
-//   .then(() => {
-//     server.listen(PORT, () => {
-//       console.log(`Server listening on port ${PORT}`);
-//     });
-//   })
-//   .catch((error) => console.error(error));
-
-  
-
-
-
-  const axios = require("axios");
+const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
 require("dotenv").config();
 
 const { PORT } = process.env;
-
-async function startServer() {
-  try {
-
-    await conn.sync();
-
+conn
+  .sync()
+  .then(() => {
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
     });
+  })
+  .catch((error) => console.error(error));
 
-  } catch (error) {
-
-    console.log("Database not ready, retrying in 5 seconds...");
-    console.error(error.message);
-
-    setTimeout(startServer, 5000);
-
-  }
-}
-
-startServer();
